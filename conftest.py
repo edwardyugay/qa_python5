a42aa2b6128fe5c486c34f293b3f3856f1784006
edwardyugay <edwardehwa@gmail.com>
2025-03-26 23:21:53 +0900

    Update conftest.py
    Убрал функцию ожидания в Helper.py

--- conftest.py
+++ conftest.py
@@ -2,8 +2,6 @@
 
 import pytest
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture(scope="function")
 def driver(request):
@@ -19,13 +17,4 @@ def driver(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="Тип браузера: chrome или firefox")
 
-def wait_for_element(driver, by, locator, timeout=10):
-    """
-    Ждет появления элемента на странице до timeout секунд.
-    :param driver: экземпляр WebDriver
-    :param by: способ поиска (например, "xpath")
-    :param locator: значение локатора
-    :param timeout: время ожидания (по умолчанию 10 секунд)
-    :return: найденный элемент или выбросит TimeoutException
-    """
-    return WebDriverWait(driver, timeout).until(EC.presence_of_element_located((by, locator)))
+
